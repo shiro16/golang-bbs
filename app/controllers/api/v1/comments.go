a5fd8bf83f46f343bc40fe12ab8d4a8d0f7497be
shiro16 <nyanyanyawan24@gmail.com>
2015-08-13 21:41:06 +0900

    add processing of model

--- app/controllers/api/v1/comments.go
+++ app/controllers/api/v1/comments.go
@@ -2,6 +2,9 @@ package controllers
 
 import (
 	"github.com/revel/revel"
+	"github.com/shiro16/golang-bbs/app/controllers"
+	"github.com/shiro16/golang-bbs/app/models"
+	"gopkg.in/validator.v2"
 )
 
 type ApiV1Comments struct {
@@ -9,21 +12,57 @@ type ApiV1Comments struct {
 }
 
 func (c ApiV1Comments) Index() revel.Result {
-	r := Response{"index"}
+	comments := []models.Comment{}
+
+	if err := controllers.DB.Order("id desc").Find(&comments).Error; err != nil {
+		return c.HandleInternalServerError("Record Find Failure")
+	}
+
+	r := Response{comments}
 	return c.RenderJson(r)
 }
 
 func (c ApiV1Comments) Show(id int) revel.Result {
-	r := Response{"show"}
+	comment := &models.Comment{}
+
+	if err := controllers.DB.First(&comment, id).Error; err != nil {
+		return c.HandleNotFoundError(err.Error())
+	}
+
+	r := Response{comment}
 	return c.RenderJson(r)
 }
 
 func (c ApiV1Comments) Create() revel.Result {
-	r := Response{"create"}
+	comment := &models.Comment{}
+
+	if err := c.BindParams(comment); err != nil {
+		return c.HandleBadRequestError(err.Error())
+	}
+
+	if err := validator.Validate(comment); err != nil {
+		return c.HandleBadRequestError(err.Error())
+	}
+
+	if err := controllers.DB.Create(comment).Error; err != nil {
+		return c.HandleInternalServerError("Record Create Failure")
+	}
+
+	r := Response{comment}
 	return c.RenderJson(r)
 }
 
 func (c ApiV1Comments) Delete(id int) revel.Result {
-	r := Response{"delete"}
+	comment := models.Comment{}
+
+	if err := controllers.DB.First(&comment, id).Error; err != nil {
+		return c.HandleNotFoundError(err.Error())
+	}
+
+	if err := controllers.DB.Delete(&comment).Error; err != nil {
+		return c.HandleInternalServerError("Record Delete Failure")
+	}
+
+	r := Response{"success"}
 	return c.RenderJson(r)
 }
